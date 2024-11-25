3f8eade54017a3d322eda1fa2f71c12b5b549126
ibrahim06978 <ibrahim06978@gmail.com>
2024-11-25 11:38:05 +0600

    done

--- src/Components/Item.jsx
+++ src/Components/Item.jsx
@@ -10,7 +10,7 @@ function Item({item}) {
   const {setId} = useContext(ProductContext)
     return (
         <div  onClick={() => setId(item.id)} className=" col-span-4 cursor-pointer  w-full max-h-[300px] transform overflow-hidden rounded-lg bg-white dark:bg-slate-800 shadow-md duration-300 hover:scale-105 hover:shadow-lg">
-            <img className="h-[150px] w-full object-cover object-center" src={item.image} alt="Product Image" />
+            <img className="h-[150px] lg:w-[400px] xl:w-[500px] object-cover object-center" src={item.image} alt="Product Image" />
                 <h2 className="mb-2 mt-2 px-1 text-lg lg:text-xs font-medium text-nowrap dark:text-white text-gray-900">{item.title.slice(0,20)}...</h2>
             <div className="p-4">
                 
